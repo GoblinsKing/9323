06347b9a56e1b954483f5ab8329bc3cbbd736115
GoblinsKing <36937872+GoblinsKing@users.noreply.github.com>
2019-11-13 00:49:01 +1100

    finish auth

--- backend/apis/auth.py
+++ backend/apis/auth.py
@@ -5,19 +5,6 @@ import db.init_db as db
 
 api = Namespace('auth', description='Authentication Services')
 
-login_details = api.model('login_details', {
-  'username': fields.String(required=True, example='SampleUsername'),
-  'password': fields.String(required=True, example='SamplePassword'),
-})
-
-signup_details = api.model('signup_details', {
-  'username': fields.String(required=True, example='SampleUsername'),
-  'password': fields.String(required=True, example='SamplePassword'),
-  'email': fields.String(required=True, example='peter@gmail.com'),
-  'phone_number': fields.String(required=True, example='0412345678'),
-  'name':  fields.String(required=True, example='Peter Snow')
-})
-
 @api.route('/login')
 class Login(Resource):
     @api.response(200, 'Success')
@@ -32,42 +19,13 @@ class Login(Resource):
     def post(self):
         if not request.json:
             abort(400, 'Malformed Request')
-        (username, password) = unpack(request.json, 'username', 'password')
+        (zid, password) = unpack(request.json, 'zid', 'password')
         session = db.get_session()
-        user = session.query(db.User).filter_by(username=username, password=password).first()
+        user = session.query(db.User).filter_by(zid=zid, password=password).first()
         if not user:
             abort(403,'Invalid Username/Password')
         t = gen_token()
         user.token = t
-        session.commit()
-        session.close()
-        return {
-            'token': t
-        }
-
-@api.route('/signup')
-class Signup(Resource):
-    @api.response(200, 'Success')
-    @api.response(400, 'Malformed Request')
-    @api.response(409, 'Username Taken')
-    @api.expect(signup_details)
-    @api.doc(description='''
-        Use this endpoint to create a new account,
-        username must be unique and password must be non empty
-        After creation api retuns a auth token, same as /login would
-    ''')
-    def post(self):
-        if not request.json:
-            abort(400, 'Malformed Request')
-        (username, password, email, phone_number, name) = unpack(request.json, 'username', 'password', 'email', 'phone_number', 'name')
-        if username == '' or password == '':
-            abort(400, 'Username or Password cannot be empty')
-        session = db.get_session()
-        if session.query(db.User).filter_by(username=username).first():
-            abort(409, 'Username Taken')
-        t = gen_token()
-        new_user = db.User(token = t, username = username, password = password, email = email, phone_number = phone_number, name = name)
-        session.add(new_user)
         session.commit()
         session.close()
         return {
--- backend/test.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-with open(os.path.join('users.csv')) as f:
-    for line in f.readlines():
-        line = line.strip().split(',')
-        print(line[2])
